7311855559bcb8f08aaedc0916f39a1e98acec02
user <user@interview-test-machine.(none)>
2014-02-28 16:43:52 -0800

    empty project

---
+++ src/main/java/com/kaseya/java_interview_test/java_interview_test/App.java
@@ -0,0 +1,13 @@
+package com.kaseya.java_interview_test.java_interview_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
